54ac63c647ac71c0aec434f8b22b0361ca230726
khartono <crystalicclear@hotmail.com>
2016-04-28 02:25:25 +0000

    Does this work?

--- BookTable/script.js
+++ BookTable/script.js
@@ -1,4 +1,8 @@
-$(".ATLS").mouseenter(function() {
-    $(".ATLS").html("Atlas Shrugged by Ayn Rand");
+var myBooks = ["atls1.jpg", "atls2.jpg", "atls3.jpg", "atls4.jpg", "atls5.jpg", "atls6.jpg", "atls7.jpg", "atls8.jpg"];
+
+$(".mybutton").click(function() {
+    console.log("clicked");
+    var newimg = "<img src='covers/ATLS/" + myBooks[Math.floor(Math.random() * myBooks.length)] +  "'>";
+    $(".container").prepend(newimg);
     
 })
---
+++ ruby/example.rb
@@ -0,0 +1,14 @@
+def sayHello (numberOfTimes, anotherParam)
+    puts "Hello World" * numberOfTimes
+    puts anotherParam
+end
+
+sayHello(5, "BITCH")
+
+def addNums(num1, num2)
+    sum = num1 + num2
+    puts sum
+end
+
+addNums(2, 8)
+
