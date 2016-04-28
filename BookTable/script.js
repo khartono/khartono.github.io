14fa0360a26d686b644fe72400fdb7f2748fc12c
khartono <crystalicclear@hotmail.com>
2016-04-28 12:37:47 +0000

    hey

--- BookTable/script.js
+++ BookTable/script.js
@@ -1,4 +1,10 @@
-var myBooks = ["atls1.jpg", "atls2.jpg", "atls3.jpg", "atls4.jpg", "atls5.jpg", "atls6.jpg", "atls7.jpg", "atls8.jpg"];
+$(".years").click(function() {
+    console.log("clicked");
+    $(".years").addClass($(".selection-2"))
+    
+})
+
+var myBooks = ["atls1.jpg", "atls2.jpg", "atls3.jpg", "atls4.jpg", "atls5.jpg", "atls6.jpg", "atls7.jpg", "atls8.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "CWO1.jpg", "CWO2.jpg"];
 
 $(".mybutton").click(function() {
     console.log("clicked");
